644235aec67eb98d54d732ca2c7541791b5dc6fd
Peter Kim <peter.kim@uconn.edu>
2022-10-27 22:56:51 -0400

    Get routes for landing page and notes page

---
+++ Develop/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
