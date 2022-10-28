ffe109997e840bd20ea99d66f6a3c90e3e72d845
Peter Kim <peter.kim@uconn.edu>
2022-10-28 01:55:21 -0400

    import fs, path, db

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
-
+const uuid = require('./helper/uuid');
+const database = require('./db/db.json');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 
 app.use(express.static('public'));
@@ -9,9 +12,15 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('/notes', (req, res) =>
+app.get('/api/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a review`);
+});
+
+
 
 app.listen(PORT, () => 
     console.log(`App listening at http://localhost:${PORT}`)
